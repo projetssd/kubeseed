f070741e561043d87620a7f286778b3aebe3584b
Merrick28 <stephane.dewitte@gmail.com>
2023-02-24 11:50:41 +0100

    bugfix

--- includes/variables.sh
+++ includes/variables.sh
@@ -52,7 +52,7 @@ fi
 
 export SETTINGS_SOURCE=${SETTINGS_SOURCE}
 export SETTINGS_STORAGE=${SETTINGS_STORAGE}
-export VENV_DIR=${SETTINGS_STORAGE}/venv/k3s
+export VENV_DIR=${SETTINGS_STORAGE}/venv/ks
 if [ -f "${VENV_DIR}/bin/activate" ]; then
   temppath=$(ls ${VENV_DIR}/lib)
   pythonpath=${VENV_DIR}/lib/${temppath}/site-packages
