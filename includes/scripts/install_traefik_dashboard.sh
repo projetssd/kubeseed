d6bdeffabcf7a781c0809a5a27fc5a3242f1dab7
Merrick28 <stephane.dewitte@gmail.com>
2023-02-28 16:44:08 +0100

    Ajout des secrets

--- includes/scripts/install_traefik_dashboard.sh
+++ includes/scripts/install_traefik_dashboard.sh
@@ -11,6 +11,7 @@ read -rp $'\e[33mQuel sous domaine pour le dashboard Traefik [traefik]\e[0m :' t
 traefik_domain=${dtraefik_domain:-traefik}
 ks_manage_account_yml appli.traefik.domain ${traefik_domain}
 ansible-playbook "${SETTINGS_SOURCE}/includes/playbooks/cloudflare_domain.yml" -e "subdomain=${traefik_domain}"
+ansible-playbook "${SETTINGS_SOURCE}/includes/playbooks/k3s_create_secret.yml"
 ansible-playbook "${SETTINGS_SOURCE}/includes/playbooks/traefik_dashboard.yml"
 echo "==================================="
 echo "Le dashboard est accessible à l'adresse https://${traefik_domain}.$(ks_get_from_account_yml user.domain)/dashboard/"
